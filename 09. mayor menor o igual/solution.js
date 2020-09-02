b72659a71ae7ef78bb403a73744f8d9016c7e5a7
4rturd13 <4rturd13@gmail.com>
2020-09-02 16:24:37 -0500

    resolve exercise 9

---
+++ 09. mayor menor o igual/solution.js
@@ -0,0 +1,6 @@
+const num = Number(prompt("Insert a number"));
+num < 10
+  ? console.log("The number is less than 10")
+  : num > 10
+  ? console.log("The number is greater than 10")
+  : console.log("The number is equal to 10");
