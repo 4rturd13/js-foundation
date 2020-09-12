3669488e0408fc3a80069694318638f0224f48d0
4rturd13 <4rturd13@gmail.com>
2020-09-11 20:28:04 -0500

    resolve exercise 51

---
+++ 51. numeros pares (filter)/solution.js
@@ -0,0 +1,5 @@
+const pairs = (arr) => arr.filter((num) => num % 2 === 0);
+
+console.log(pairs([0, 1, 2, 3, 4]));
+console.log(pairs([120, 876, 934, 1298]));
+console.log(pairs([7, 921, 43, 9649]));
